0bce20bef4aef83440fb9d82c2f2256e8e96d634
Lukman Sanni <lukmansanni60@gmail.com>
2021-01-15 01:59:03 +0100

    add production chart component

--- src/components/ChartBox/ChartBox.jsx
+++ src/components/ChartBox/ChartBox.jsx
@@ -4,27 +4,29 @@ import React from 'react';
 import styles from './ChartBox.module.scss';
 
 import HeadingSecondary from '../HeadingSecondary/HeadingSecondary';
-import { ReactComponent as ReloadSvg} from '../../assets/svgs/loading.svg';
-import { ReactComponent as CancelSvg} from '../../assets/svgs/cancel.svg';
-import { ReactComponent as ArrowDownSvg} from '../../assets/svgs/arrowD.svg';
+import { ReactComponent as ReloadSvg } from '../../assets/svgs/loading.svg';
+import { ReactComponent as CancelSvg } from '../../assets/svgs/cancel.svg';
+import { ReactComponent as ArrowDownSvg } from '../../assets/svgs/arrowD.svg';
 
 
-const ChartBox = props =>{
+const ChartBox = props => {
 
-    const {title, hasIcons} = props;
-    
+    const { title, hasIcons, children } = props;
+
     const svgs = <> <ReloadSvg /> <CancelSvg /> <ArrowDownSvg /> </>;
 
     return (
-        
+
         <div className={styles.box}>
-           <div className={styles.titleBox}>
-           <HeadingSecondary>{title}</HeadingSecondary>
-          
-            {
-                hasIcons && <div className={styles.icons}> {svgs} </div>
-            }
-           </div>
+            <div className={styles.titleBox}>
+
+                <HeadingSecondary>{title}</HeadingSecondary>
+
+                {hasIcons && <div className={styles.icons}> {svgs} </div>}
+            </div>
+
+        {/* CHART ELEMENT */}
+           {children}
         </div>
     )
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,6 +14,157 @@ import { ReactComponent as MoreSVG } from "../../assets/svgs/more.svg";
 
 import ChartBox from '../../components/ChartBox/ChartBox';
 
+import FusionCharts from "fusioncharts";
+import charts from "fusioncharts/fusioncharts.charts";
+import ReactFusioncharts from "react-fusioncharts";
+
+// Resolves charts dependancy
+charts(FusionCharts);
+
+const dataSource = {
+  chart: {
+    "showZeroPlane": "0",
+    "showBorder": "0",
+    "bgColor": "#D5D5D5",
+    "bgAlpha": "100",
+    "canvasbgColor": "#D5D5D5",
+    "canvasBorderThickness": "0",
+    "showAlternateHGridColor": "0",
+    numdivlines: "8",
+    adjustdiv: "0",
+    syaxismaxvalue: "5",
+    syaxisminvalue: "-3",
+    yaxismaxvalue: "30",
+    theme: "fusion",
+    drawcustomlegendicon: "0",
+    plottooltext: "$label, $seriesname: $dataValue",
+    palettecolors: "#f0a51b,#D5D5D5,#5ea9d8",
+  },
+  categories: [
+    {
+      category: [
+        {
+          label: "1"
+        },
+        {
+          label: "5"
+        },
+        {
+          label: "10"
+        },
+        {
+          label: "15"
+        },
+        {
+          label: "25"
+        },
+        {
+          label: "30"
+        }
+      ]
+    }
+  ],
+  dataset: [
+    {
+      dataset: [
+        {
+          data: [
+            {
+              value: "-7"
+            },
+            {
+              value: "-5"
+            },
+            {
+              value: "-6"
+            },
+            {
+              value: "-8"
+            },
+            {
+              value: "-6"
+            },
+            {
+              value: "-5"
+            }
+          ]
+        },
+        {
+          data: [
+            {
+              value: "2"
+            },
+            {
+              value: "5"
+            },
+            {
+              value: "9"
+            },
+            {
+              value: "7"
+            },
+            {
+              value: "8"
+            },
+            {
+              value: "4"
+            }
+          ]
+        },
+        {
+          data: [
+            {
+              value: "22"
+            },
+            {
+              value: "21"
+            },
+            {
+              value: "13"
+            },
+            {
+              value: "21"
+            },
+            {
+              value: "10"
+            },
+            {
+              value: "17"
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  lineset: [
+    {
+      showvalues: "0",
+      color: "#FDCB50",
+      anchorbgcolor: "#FDCB50",
+      showanchors: "0",
+      data: [
+        {
+          value: "1"
+        },
+        {
+          value: "1"
+        },
+        {
+          value: "3.2"
+        },
+        {
+          value: "1.2"
+        },
+        {
+          value: "0.5"
+        },
+        {
+          value: "0.2"
+        }
+      ]
+    }
+  ]
+};
 
 const Home = () => {
 
@@ -27,7 +178,7 @@ const Home = () => {
           detailName="total profit"
           detailPercent="+4.8%"
           Svg={DataSVG}
-          bg= {colors.blue}
+          bg={colors.blue}
         />
 
         <StatsBox
@@ -35,7 +186,7 @@ const Home = () => {
           detailName="active users"
           detailPercent="+7.8%"
           Svg={UserSVG}
-          bg= {colors.green}
+          bg={colors.green}
         />
 
         <StatsBox
@@ -43,23 +194,31 @@ const Home = () => {
           detailName="new orders"
           detailPercent="+3.7%"
           Svg={StacksSVG}
-          bg= {colors.orange}
+          bg={colors.orange}
         />
-        
+
         <StatsBox
           detailStat="12"
           detailName="open complaints"
           detailPercent="-6.5%"
           Svg={MoreSVG}
-          bg= {colors.red}
+          bg={colors.red}
         />
-    
+
       </div>
 
       <section className={styles.charts}>
-      <ChartBox title='Prodction' hasIcons={true} />
-      <ChartBox title='sales revenue by country' />
-      <ChartBox title='summary'  hasIcons={true} />
+        <ChartBox title='Prodction' hasIcons={true}>
+          <ReactFusioncharts
+            type="msstackedcolumn2dlinedy"
+            width="100%"
+            height="70%"
+            dataFormat="JSON"
+            dataSource={dataSource}
+          />
+        </ChartBox>
+        <ChartBox title='sales revenue by country' />
+        <ChartBox title='summary' hasIcons={true} />
       </section>
 
     </>
